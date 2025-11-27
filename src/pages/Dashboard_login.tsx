a4dc6a10d1d2cf7504d6eedbd66ad6d75c604527
FrancoNotebook <francodorrego@gmail.com>
2025-11-27 00:28:36 +0000

    mejora de login

--- src/pages/Dashboard_login.tsx
+++ src/pages/Dashboard_login.tsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import Cookies from 'js-cookie';
+import { AUTH_COOKIE_NAME } from '@/lib/api';
 
 const DashboardLogin = () => {
   const navigate = useNavigate();
@@ -11,7 +12,19 @@ const DashboardLogin = () => {
   
   useEffect(() => {
     if (!isLoading && isAuthenticated) {
-        setTimeout( ()=> {navigate('/panel', { replace: true });}, 300); // Pequeño retraso para asegurar que la cookie se ha establecido)
+        // Me quedo a preguntar si tengo la cookie de Autenticacion y si no pregunto cada 100ms hasta que la tenga
+        const authCookie = Cookies.get(AUTH_COOKIE_NAME);
+        if (!authCookie) {
+            const interval = setInterval(() => {
+                const checkCookie = Cookies.get(AUTH_COOKIE_NAME);
+                if (checkCookie) {
+                    clearInterval(interval);
+                    navigate('/panel', { replace: true });
+                }
+            }, 100);
+        } else {
+            navigate('/panel', { replace: true });
+        }
     }
   }, [isLoading, isAuthenticated, navigate]);
 
@@ -20,7 +33,7 @@ const DashboardLogin = () => {
     <div className="flex items-center justify-center min-h-screen">
       <div className="text-center">
         <div className="loader mb-4"></div> {/* Aquí puedes poner una animación de carga */}
-        <p className="text-lg font-medium">Cargando tu panel de control...</p>
+        <p className="text-lg font-medium">Cargando tu panel...</p>
       </div>
     </div>
   );
