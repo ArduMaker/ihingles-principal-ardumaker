1b608a68bfedc83839db1c80cb0c3542f6b09699
FrancoNotebook <francodorrego@gmail.com>
2025-11-27 00:32:47 +0000

    login

--- src/pages/Dashboard_login.tsx
+++ src/pages/Dashboard_login.tsx
@@ -8,15 +8,21 @@ import { AUTH_COOKIE_NAME } from '@/lib/api';
 
 const DashboardLogin = () => {
   const navigate = useNavigate();
-  const { isAuthenticated, isLoading } = useAuth0();
+  const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
   
   useEffect(() => {
     if (!isLoading && isAuthenticated) {
-        // Me quedo a preguntar si tengo la cookie de Autenticacion y si no pregunto cada 100ms hasta que la tenga
+        // solo pregunto 10 veces y si no mando a loginWithRedirect
         const authCookie = Cookies.get(AUTH_COOKIE_NAME);
         if (!authCookie) {
+            let attempts = 0;
             const interval = setInterval(() => {
                 const checkCookie = Cookies.get(AUTH_COOKIE_NAME);
+                attempts++;
+                if (attempts >= 10) {
+                    clearInterval(interval);
+                    loginWithRedirect();
+                }
                 if (checkCookie) {
                     clearInterval(interval);
                     navigate('/panel', { replace: true });
@@ -25,6 +31,9 @@ const DashboardLogin = () => {
         } else {
             navigate('/panel', { replace: true });
         }
+    }
+    if (!isLoading && !isAuthenticated) {
+        loginWithRedirect();
     }
   }, [isLoading, isAuthenticated, navigate]);
 
